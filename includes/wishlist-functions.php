c28417d6fab1b5d8998a42b1fdd7df4d39a4f9b5
olivierblanc42 <olivierhumanbooster@gmail.com>
2026-07-31 10:01:28 +0200

    add function to display wishlist button

--- includes/wishlist-functions.php
+++ includes/wishlist-functions.php
@@ -94,7 +94,7 @@ function wishlist_button(int $postId)
         }
     }
 
-    echo '<button class=" btn-wishlist button-login-required' . esc_attr($class) . '" data-post-id="' . esc_attr($postId) . '"></button>';
+    echo '<button class="btn-wishlist button-login-required' . esc_attr($class) . '" data-post-id="' . esc_attr($postId) . '"></button>';
 }
 
 
@@ -147,4 +147,8 @@ function display_wishlist()
             array('class' => 'bounce_button')
         );
     }
+}
+
+function     display_wishlist_button(){
+     wishlist_button(get_the_ID());
 }
